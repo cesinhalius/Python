d08ddd99ed21580e556e4bccef81551df632c2ea
Cesar Augusto <cesinhalius@hotmail.com>
2023-06-15 18:36:34 -0300

    Adicionando funções aos botões para executar operação matematica

--- calculadora/main.py
+++ calculadora/main.py
@@ -47,24 +47,49 @@ def botao_multiplica():
     global multiplica
     multiplica = TRUE
     numero1 = e.get()
-    e.delete(0,END)
+    e.delete(0, END)
+
 
 def botao_adiciona():
     global numero1
     global adicao
     adicao = TRUE
     numero1 = e.get()
-    e.delete(0,END)
+    e.delete(0, END)
+
+
 def botao_subtracao():
     global numero1
     global subtracao
     subtracao = TRUE
     numero1 = e.get()
-    e.delete(0,END)
+    e.delete(0, END)
+
+
 def botao_igual():
-    return
+    global adicao
+    global subtracao
+    global divisao
+    global multiplica
+    numero2 = e.get()
+    e.delete(0, END)
+
+    if adicao == TRUE:
+        e.insert(0, int(numero1) + int(numero2))
+        adicao = FALSE
+    if multiplica == TRUE:
+        e.insert(0, int(numero1) * int(numero2))
+        multiplica = FALSE
+    if subtracao == TRUE:
+        e.insert(0, int(numero1) - int(numero2))
+        subtracao = FALSE
+    if divisao == TRUE:
+        e.insert(0, int(numero1) / int(numero2))
+        divisao = FALSE
+    
 def botao_limpa():
-    return
+    e.delete(0,END)
+
 
 divide = Button(root,
                 text='/',
@@ -77,107 +102,107 @@ divide = Button(root,
                 activebackground='#240046',
                 relief=FLAT,
                 font=('futura', 12, 'bold')
-)
+                )
 divide.grid(row=0, column=4)
-#primeira fileira
+# primeira fileira
 
 um = Button(root,
-                text='1',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(1),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+            text='1',
+            padx=40,
+            pady=20,
+            command=lambda: botao_click(1),
+            fg='#ffffff',
+            activeforeground='#ffffff',
+            bg='#320064',
+            activebackground='#240046',
+            relief=FLAT,
+            font=('futura', 12, 'bold')
+            )
 um.grid(row=1, column=1)
 
 dois = Button(root,
-                text='2',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(2),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='2',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(2),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 dois.grid(row=1, column=2)
 
 tres = Button(root,
-                text='3',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(3),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='3',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(3),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 tres.grid(row=1, column=3)
 
 multiplica = Button(root,
-                text='*',
-                padx=40,
-                pady=20,
-                command=botao_multiplica,
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+                    text='*',
+                    padx=40,
+                    pady=20,
+                    command=botao_multiplica,
+                    fg='#ffffff',
+                    activeforeground='#ffffff',
+                    bg='#320064',
+                    activebackground='#240046',
+                    relief=FLAT,
+                    font=('futura', 12, 'bold')
+                    )
 multiplica.grid(row=1, column=4)
-#segunda fileira
+# segunda fileira
 
 quatro = Button(root,
                 text='4',
                 padx=40,
                 pady=20,
-                command=lambda:botao_click(4),
+                command=lambda: botao_click(4),
                 fg='#ffffff',
                 activeforeground='#ffffff',
                 bg='#320064',
                 activebackground='#240046',
                 relief=FLAT,
                 font=('futura', 12, 'bold')
-)
+                )
 quatro.grid(row=2, column=1)
 
 cinco = Button(root,
-                text='5',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(5),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+               text='5',
+               padx=40,
+               pady=20,
+               command=lambda: botao_click(5),
+               fg='#ffffff',
+               activeforeground='#ffffff',
+               bg='#320064',
+               activebackground='#240046',
+               relief=FLAT,
+               font=('futura', 12, 'bold')
+               )
 cinco.grid(row=2, column=2)
 
 seis = Button(root,
-                text='6',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(6),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='6',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(6),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 seis.grid(row=2, column=3)
 
 adicao = Button(root,
@@ -191,107 +216,107 @@ adicao = Button(root,
                 activebackground='#240046',
                 relief=FLAT,
                 font=('futura', 12, 'bold')
-)
+                )
 adicao.grid(row=2, column=4)
-#terceira fileira
+# terceira fileira
 
 sete = Button(root,
-                text='7',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(7),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='7',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(7),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 sete.grid(row=3, column=1)
 
 oito = Button(root,
-                text='8',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(8),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='8',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(8),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 oito.grid(row=3, column=2)
 
 nove = Button(root,
-                text='9',
-                padx=40,
-                pady=20,
-                command=lambda:botao_click(9),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='9',
+              padx=40,
+              pady=20,
+              command=lambda: botao_click(9),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 nove.grid(row=3, column=3)
 
 subtracao = Button(root,
-                text='-',
-                padx=41,
-                pady=20,
-                command=botao_subtracao,
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+                   text='-',
+                   padx=41,
+                   pady=20,
+                   command=botao_subtracao,
+                   fg='#ffffff',
+                   activeforeground='#ffffff',
+                   bg='#320064',
+                   activebackground='#240046',
+                   relief=FLAT,
+                   font=('futura', 12, 'bold')
+                   )
 subtracao.grid(row=3, column=4)
 
 # quarta fileira
 zero = Button(root,
-                text='0',
-                padx=91,
-                pady=20,
-                command=lambda:botao_click(0),
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+              text='0',
+              padx=91,
+              pady=20,
+              command=lambda: botao_click(0),
+              fg='#ffffff',
+              activeforeground='#ffffff',
+              bg='#320064',
+              activebackground='#240046',
+              relief=FLAT,
+              font=('futura', 12, 'bold')
+              )
 zero.grid(row=4, column=1, columnspan=2)
 
 limpa = Button(root,
-                text='C',
-                padx=40,
-                pady=20,
-                command=botao_limpa,
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+               text='C',
+               padx=40,
+               pady=20,
+               command=botao_limpa,
+               fg='#ffffff',
+               activeforeground='#ffffff',
+               bg='#320064',
+               activebackground='#240046',
+               relief=FLAT,
+               font=('futura', 12, 'bold')
+               )
 limpa.grid(row=4, column=4)
 
 igual = Button(root,
-                text='=',
-                padx=40,
-                pady=20,
-                command=botao_igual,
-                fg='#ffffff',
-                activeforeground='#ffffff',
-                bg='#320064',
-                activebackground='#240046',
-                relief=FLAT,
-                font=('futura', 12, 'bold')
-)
+               text='=',
+               padx=40,
+               pady=20,
+               command=botao_igual,
+               fg='#ffffff',
+               activeforeground='#ffffff',
+               bg='#320064',
+               activebackground='#240046',
+               relief=FLAT,
+               font=('futura', 12, 'bold')
+               )
 igual.grid(row=4, column=3)
 
 root.mainloop()
