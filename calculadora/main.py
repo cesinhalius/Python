7940aa4feed03c27b83d6c6f5d07e04824ad3712
Cesar Augusto <cesinhalius@hotmail.com>
2023-06-15 16:40:19 -0300

    Adicionando os botão da primeira fileira

--- calculadora/main.py
+++ calculadora/main.py
@@ -20,8 +20,90 @@ e.grid(
 
 )
 
+#Função operedores
 
+def botao_divisao():
+    return
+def botao_click():
+    return
+def botao_multiplica():
+    return
+def botao_adiciona():
+    return
+def botao_subtracao():
+    return
 
+divide = Button(root,
+                text='/',
+                padx=40,
+                pady=20,
+                command=botao_divisao,
+                fg='#ffffff',
+                activeforeground='#ffffff',
+                bg='#320064',
+                activebackground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+divide.grid(row=0, column=4)
+#primeira fileira
+
+um = Button(root,
+                text='1',
+                padx=40,
+                pady=20,
+                command=lambda:botao_click(1),
+                fg='#ffffff',
+                activeforeground='#ffffff',
+                bg='#320064',
+                activebackground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+um.grid(row=1, column=1)
+
+dois = Button(root,
+                text='2',
+                padx=40,
+                pady=20,
+                command=lambda:botao_click(2),
+                fg='#ffffff',
+                activeforeground='#ffffff',
+                bg='#320064',
+                activebackground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+dois.grid(row=1, column=2)
+
+tres = Button(root,
+                text='3',
+                padx=40,
+                pady=20,
+                command=lambda:botao_click(3),
+                fg='#ffffff',
+                activeforeground='#ffffff',
+                bg='#320064',
+                activebackground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+tres.grid(row=1, column=3)
+
+multiplica = Button(root,
+                text='*',
+                padx=40,
+                pady=20,
+                command=botao_multiplica,
+                fg='#ffffff',
+                activeforeground='#ffffff',
+                bg='#320064',
+                activebackground='#240046',
+                relief=FLAT,
+                font=('futura', 12, 'bold')
+)
+multiplica.grid(row=1, column=4)
+#primeira fileira
 
 
 
