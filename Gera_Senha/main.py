1ee34a063d8be6901ae5d1e7fbc8018e2c2f7258
Cesar Augusto <cesinhalius@hotmail.com>
2023-06-16 19:04:19 -0300

    Adicionando função de sorteio de letras e caracteres especiais para a senha

--- Gera_Senha/main.py
+++ Gera_Senha/main.py
@@ -4,7 +4,7 @@ import random
 
 letra_maiuscula = chr(random.randint(65, 90))
 letra_minuscula = chr(random.randint(97, 122))
-char_especial = chr(33)
+char_especial = chr(64)
 
 lista_senha = []
 
@@ -12,6 +12,9 @@ lista_senha = []
 for i in range(6):
     numeros = random.randrange(9)
     lista_senha.append(numeros)
+    
 random.shuffle(lista_senha)
+lista_senha = str(lista_senha) [1:-1]
+lista_senha = lista_senha.replace(',','')
 
-print(lista_senha)
+print(letra_maiuscula, letra_minuscula , lista_senha , char_especial)
