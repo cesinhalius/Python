ecbe7f1eafa22ed42b5d7098257d07ae2f60fde4
Cesar Augusto <cesinhalius@hotmail.com>
2023-07-20 09:54:14 -0300

    Projeto de como concatenar String em Python

---
+++ Concatenacao_String/StringConcatenation.py
@@ -0,0 +1,8 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to _____"
+
+youtuber = "youtube channel"
+
+print("subscribe to" + youtuber)
+print("subscribe to{}".format(youtuber))
+print(f"subscribe to{youtuber}")
