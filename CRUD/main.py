163ed916c27df2cd68371e31a03576d26159fac6
Cesar Augusto <cesinhalius@hotmail.com>
2023-07-27 17:17:08 -0300

    Adicionando verificação de cadastro

--- CRUD/main.py
+++ CRUD/main.py
@@ -1,7 +1,14 @@
 import streamlit as st;
+from os import write;
 
 with st.form(key="include_client"):
     input_name = st.text_input(label="Digite seu nome:")
     input_age = st.number_input(label="Digite sua idade:", format="%d", step=1)
     input_occupation = st.selectbox("Selecione sua profissão" , ["Desenvolvedor", "Musico", "Arquiteto", "Engenheiro","Advogado"])
     input_submit = st.form_submit_button("Enviar")
+
+if input_submit:
+    st.write(f'Nome: {input_name}')
+    st.write(f'Idade: {input_age}')
+    st.write(f'Profissão: {input_occupation}')
+    
